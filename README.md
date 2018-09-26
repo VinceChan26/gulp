07556e3fd8199be2fa721c259a6073c9d594978a
Vince <vince.chanl26@gmail.com>
2018-09-26 15:03:10 +0800

    Add | gulpfile initial

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # gulp
+
+> 基於 gulp, gulp-concat, gulp-clean-css, gulp-uglify, gulp-rename 的範例
+
+
+## Usage
+
+Firstly, install the package from [npm](https://www.npmjs.com/)
+
+```
+npm install gulp gulp-concat gulp-clean-css gulp-uglify gulp-rename --save-dev
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,39 @@
+let gulp = require('gulp');
+let concat = require('gulp-concat');
+let cleanCSS = require('gulp-clean-css');
+let uglify = require('gulp-uglify');
+let rename = require("gulp-rename");
 
+gulp.task('cssBuilder', () => {
+return gulp.src(['./css/*.css', './css/*.css'])
+  .pipe(concat('bundles.css'))
+  .pipe(cleanCSS())
+  .pipe(rename(function(path) {
+    path.basename += ".min";
+    path.extname = ".css";
+  }))
+  .pipe(gulp.dest('./public/assets/css/'));
+});
+
+gulp.task('JSuglify', function() {
+  return gulp.src(['./js/*.js'])
+    .pipe(uglify())
+    .pipe(rename(function(path) {
+      path.basename += ".min";
+      path.extname = ".js";
+    }))
+    .pipe(gulp.dest('./public/assets/js/'));
+});
+
+gulp.task('uglify-concat', function() {
+  return gulp.src(['./js/*.js'])
+    .pipe(concat('bundles.js'))
+    .pipe(uglify())
+    .pipe(rename(function(path) {
+      path.basename += ".min";
+      path.extname = ".js";
+    }))
+    .pipe(gulp.dest('./public/assets/js/'));
+});
+
+gulp.task('default', ['cssBuilder', 'JSuglify', 'uglify-concat']);
